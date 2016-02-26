69208371d0aa2c5d77345f8074f5f73ffa5f22be
unknown <oleksik@ws-000329.westfalia-net.com>
2016-02-26 14:27:12 +0100

    Som Animation

--- animationLogic.js
+++ animationLogic.js
@@ -1,5 +1,7 @@
 var main = function() {
+	Boolean isOpen = false;
   $('.icon-menu').click(function() {
+  	if(isOpen){
     $('.menu').animate({
       left: "0px"
     }, 200);
@@ -7,18 +9,21 @@ var main = function() {
     $('body').animate({
       left: "285px"
     }, 200);
-  });
-
-  /* Then push them back */
-  $('.icon-close').click(function() {
-    $('.menu').animate({
+    isOpen = !isOpen;
+    }
+    else
+    {
+    	$('.menu').animate({
       left: "-285px"
     }, 200);
 
     $('body').animate({
       left: "0px"
     }, 200);
+    isOpen = !isOpen;
+    }
   });
+
 };
 
 
