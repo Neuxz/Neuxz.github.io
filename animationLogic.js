35b587656f457741f640e6ea8302eb8dade69da4
unknown <oleksik@ws-000329.westfalia-net.com>
2016-03-04 16:54:51 +0100

    Javascript read repo specific html's. css & html design for js.

--- animationLogic.js
+++ animationLogic.js
@@ -1,3 +1,14 @@
+function makeHttpObject() {
+  try {return new XMLHttpRequest();}
+  catch (error) {}
+  try {return new ActiveXObject("Msxml2.XMLHTTP");}
+  catch (error) {}
+  try {return new ActiveXObject("Microsoft.XMLHTTP");}
+  catch (error) {}
+
+  throw new Error("Could not create HTTP request object.");
+}
+var list = document.getElementById('mainContend');
 var open = function() {
 	$('.menu').animate({left: "-285px"}, 205);
 	$('.repo').animate({top: "-285px"}, 100);
@@ -14,10 +25,33 @@ var closen = function() {
     }, 200);
 } 
 
+var getHtml = function(index) {
+	var url = "http://Neuxz.github.io"
+	switch(index)
+	{
+		case 0: url += "/MangaDLEX/";
+		break;
+		case 1: url += "/RemoteCC_Client/";
+		break;
+		default: url += "/#";
+	}
+	var request = makeHttpObject();
+	request.open("GET", url, true);
+	request.send(null);
+	request.onreadystatechange = function() {
+  	if (request.readyState == 4)
+  	{
+  		$('#mainContend').empty();
+    var loadSite = document.createElement('li');
+	loadSite.insertAdjacentHTML('beforeend', request.responseText);
+	list.appendChild(loadSite);
+	}
+	};
+}
+
 var main = function() {
   var isOpen = true;
   $('.icon-menu').click(function() {
-  	console.log("IN");
   	if(isOpen){
     isOpen = !isOpen;
 	closen();
@@ -27,10 +61,9 @@ var main = function() {
     isOpen = !isOpen;
 	open();
     }
+    console.log("IN");
   });
-  $('.repo').click(function() {
-  	closen();
-  	});
+  $('.repo').click(function(){ getHtml($(this).index()); });
   $('.repo').hover(function() {
     $(this).removeClass('btn-primary').addClass('btn-warning');
     $(this).stop().animate({
